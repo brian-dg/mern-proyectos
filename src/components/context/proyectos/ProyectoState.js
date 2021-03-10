e9f2671bf45fe4ada0707ddca1fbe8c0999ae87d
brian <brian_rc6@hotmail.com>
2021-03-10 11:17:11 -0300

    llamando los proyectos con dispatch

--- src/components/context/proyectos/ProyectoState.js
+++ src/components/context/proyectos/ProyectoState.js
@@ -3,13 +3,25 @@ import React, { useReducer } from 'react';
 
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
-import {FORMULARIO_PROYECTO} from '../../types';
+import {
+    FORMULARIO_PROYECTO,
+    OBTENER_PROYECTOS } from '../../types';
+
 
 
 const ProyectoState = props => {
-    const initialState = {
-        formulario: false
-    }
+
+    const proyecto = [
+        {id:1, nombre:'Tienda Virtual'},
+        {id:2, nombre: 'Intranet'},
+        {id:3, nombre: 'Dieseño de Sitio Web'},
+        {id:4, nombre: 'MERN'}
+    ]
+        const initialState = {
+            proyecto: [],
+            formulario: false
+        }
+        
 
 
 // Dispatch para ejecutar las acciones
@@ -25,18 +37,30 @@ const mostrarFormulario = () => {
     })
 }
 
+//Obtener los proyectos
+const obtenerProyectos = () => {
+    dispatch ({
+        type: OBTENER_PROYECTOS,
+        payload: proyecto 
+    })
+}
 
 return (
 
     <proyectoContext.Provider
         value={{
+            proyecto: state.proyecto,
             formulario : state.formulario,
-            mostrarFormulario
+            mostrarFormulario,
+            obtenerProyectos
         }}>
         {props.children}
     </proyectoContext.Provider>
 )
 
 }
+
+
+
 
 export default ProyectoState;
--- src/components/proyectos/ListadoProyecto.js
+++ src/components/proyectos/ListadoProyecto.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import Proyecto from './Proyecto';
+import proyectoContext from '../context/proyectos/proyectoContext';
+
 
 
 const ListadoProyecto = () => {
+    //extraer proyectos de state inicial
 
-    const proyecto = [
 
-        {nombre:'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Dieseño de Sitio Web'}
-]
+
+    const proyectosContext = useContext(proyectoContext);
+    const {proyecto, obtenerProyectos} = proyectosContext;
+
+    //obtener proyectos cuando carga el componente
+    useEffect(() =>{
+        obtenerProyectos();
+    }, []);
+
+    // Revisar si proyecto tiene contenido 
+    if(proyecto.length === 0) return null;
+
+    
 
     return (
 
+
         <ul className="listado-proyectos">
-            {proyecto.map(proyecto => (
+            {proyecto.map( proyecto => (
                 <Proyecto
+                key={proyecto.id}
                 proyecto={proyecto}/>
             ))}
         </ul>
